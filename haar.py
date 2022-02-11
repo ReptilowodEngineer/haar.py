239511701b2a93d581fe045df6aee8cb68a0cb78
Messia1985 <messia1985@mail.ru>
2022-02-11 09:42:30 +0500

    Add files via upload

--- haar.py
+++ haar.py
@@ -1,13 +1,10 @@
 from picamera import PiCamera
+
 camera = PiCamera()
 camera.resolution = (640, 480)
 n = str(1)
-<<<<<<< HEAD
-p='/home/pi/Desktop/10Ja/' #адрес пустой папки
-=======
-p='/home/pi/Desktop/10Jan/' #адрес пустой папки
->>>>>>> origin/master
-#p = 'C:/Users/messi/Desktop/OpenCV_automobile/'
+p = '/home/pi/Desktop/10Jan/'  # адрес пустой папки
+# p = 'C:/Users/messi/Desktop/OpenCV_automobile/'
 y = 0
 import cv2
 import time
@@ -24,11 +21,10 @@ w1 = {0: 0}
 tim = 0
 q = 1
 
-bag_cascade=cv2.CascadeClassifier('/home/pi/Desktop/10Jan/cascade_bag.xml')
-cr_cascade=cv2.CascadeClassifier('/home/pi/Desktop/10Jan/cascade_cr.xml')
-qr_cascade=cv2.CascadeClassifier('C:/Users/messi/Desktop/OpenCV_automobile/qr/data/cascade.xml')
-#img camera name n=0 n=n+1 list +n if list(c)==key delete if p=0 & list not empty print list
-
+bag_cascade = cv2.CascadeClassifier('/home/pi/Desktop/10Jan/cascade_bag.xml')
+cr_cascade = cv2.CascadeClassifier('/home/pi/Desktop/10Jan/cascade_cr.xml')
+qr_cascade = cv2.CascadeClassifier('C:/Users/messi/Desktop/OpenCV_automobile/qr/data/cascade.xml')
+# img camera name n=0 n=n+1 list +n if list(c)==key delete if p=0 & list not empty print list
 
 
 """
@@ -43,12 +39,12 @@ nam = ''
 z = 1.5
 u = 1
 while True:
-    camera.capture(p+str(n)+'.jpg')
-    img=cv2.imread(p+str(n)+'.jpg')
+    camera.capture(p + str(n) + '.jpg')
+    img = cv2.imread(p + str(n) + '.jpg')
     # img=cv2.imread(p+n+'.jpg')
 
-        # camera.capture(p+str(n)+'.jpg')
-        # img=cv2.imread(p+str(n)+'.jpg')
+    # camera.capture(p+str(n)+'.jpg')
+    # img=cv2.imread(p+str(n)+'.jpg')
     h8, w8 = img.shape[:2]
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     bag = bag_cascade.detectMultiScale(gray, z, 5)
@@ -92,16 +88,15 @@ while True:
                                 y1[k] = y
                                 w1[k] = w2
                                 h1[k] = h2
-                                #cv2.imwrite(p + str(n) + '.jpg', cv2.imread(imgg))
+                                # cv2.imwrite(p + str(n) + '.jpg', cv2.imread(imgg))
                                 cr = cr_cascade.detectMultiScale(gray, 1.004, 1)
-                                
-                                
+
                                 for (x3, y3, w3, h3) in cr:
                                     s3 = (w3 + h3)
                                     s4 = (w8 + h8) / 3
                                     if s3 > s4:
                                         org = (x3, y3 + 20)
-                                        cv2.putText(img, 'Damage', org, cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 0, 255), 1)
+                                        cv2.putText(img, 'Damage', org, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
                                         cv2.rectangle(img, (x3, y3), (x3 + w3, y3 + h3), (255, 255, 0), 2)
                                         roi_gray = gray[y3:y3 + h3, x3:x3 + w3]
                                         roi_color = img[y3:y3 + h3, x3:x3 + w3]
@@ -115,16 +110,15 @@ while True:
                                         l1[k] = c
                                 q = 1
 
-                                    # When everything is done, release the capture
+                                # When everything is done, release the capture
                                 n = int(int(n) + 1)
 
-    
     cv2.startWindowThread()
     cv2.namedWindow('img')
     cv2.imshow('img', img)
     time.sleep(5)
     cv2.destroyAllWindows()
-    os.remove(str(n)+'.jpg')
+    os.remove(str(n) + '.jpg')
     print(l3)
     print(l4)
     g = 2
